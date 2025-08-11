3e12a49a8c5c70d3dc173354f1387644a31519ab
smohtadi <salmanmohtadi@gmail.com>
2025-08-10 21:10:56 -0700

    removes console.log

--- src/server/actions/game-actions.ts
+++ src/server/actions/game-actions.ts
@@ -4,6 +4,5 @@ const API_URL = process.env.NEXT_PUBLIC_SERVER_API_URL;
 export async function createRoom(): Promise<string> {
   const res = await fetch(`${API_URL}/api/create-room`, { method: "POST" });
   const data = await res.json();
-  console.log(data);
   return data.roomId;
 }
